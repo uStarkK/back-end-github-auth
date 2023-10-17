e88b5af256c35d993f252122a540956686d25093
Nahu <nahugarnica@hotmail.com>
2023-10-17 05:35:23 -0300

    Details

--- src/public/js/profile.js
+++ src/public/js/profile.js
@@ -1,7 +1,8 @@
 const API_URL = window.location.protocol + '//' + window.location.host + '/' + "api/";
 let cartId = sessionStorage.getItem("cart")
 if (!sessionStorage.getItem("cart")) {
-    fetch("http://localhost:8080/api/sessions/current", { method: 'GET' })
+    const url = API_URL + "sessions/current"
+    fetch(url, { method: 'GET' })
         .then(response => response.json())
         .then(data => {
             cartId = data.user.cart._id
